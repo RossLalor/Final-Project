efed89564bed0d0ea1baeb573bc9ba3b414fb809
RossLalor <92318461+RossLalor@users.noreply.github.com>
2023-12-02 22:47:02 +0000

    Revert "Update middleware.ts"
    This reverts commit f2d77c3bec37ac0f13b3a54f83a0b24be8c493a8.

--- final_project/middleware.ts
+++ final_project/middleware.ts
@@ -1,9 +1,9 @@
 import { authMiddleware } from "@clerk/nextjs";
 export default authMiddleware({
   // "/" will be accessible to all users
-publicRoutes: ["/"]
+publicRoutes: ["/", "/butts", "/page"]
 });
  
 export const config = {
-      matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+      matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/(api|trpc)(.*)'],
 };
