653dd1e84b2cfbdfaf717c27e5cc4815cf9f6db6
RossLalor <92318461+RossLalor@users.noreply.github.com>
2024-04-12 22:35:27 +0100

    Getting Firebase somewhat working
    Getting firebase into a state where it can add userinfo based on the clerk user id. This is automatically done when the user page is loaded (temporarily the firebaseadduser page). This was haram but it is now somewhat working. Future progress to come.

---
+++ final_project/app/firebaseAddData/page.tsx
@@ -0,0 +1,52 @@
+"use client";
+import * as React from "react";
+import { getDocs } from "firebase/firestore";
+import { initializeApp } from "firebase/app";
+import { doc, getFirestore, setDoc } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
+import { constant } from "lodash";
+import { defined } from "chart.js/helpers";
+import { useUser } from "@clerk/nextjs";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDSPoV9ip_Ti5qDbRb6l8kQmaarmunRB-A",
+  authDomain: "year4-final.firebaseapp.com",
+  projectId: "year4-final",
+  storageBucket: "year4-final.appspot.com",
+  messagingSenderId: "616758389024",
+  appId: "1:616758389024:web:bedbb56a6f128eb8698f45",
+  measurementId: "G-P2W1S16X93",
+};
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export default function SignUp() {
+  const { user } = useUser();
+
+  React.useEffect(() => {
+    async function addData() {
+      // Check if the Clerk user is loaded and has an ID
+      if (user?.id) {
+        const userId = user.id; // Clerk user ID
+        const userData = {
+          name: user.firstName,
+          // or any other data you want to store
+          // You can add more user-related data here
+        };
+        const userDocRef = doc(db, "users", userId); // Use Clerk user ID as the document ID
+
+        try {
+          await setDoc(userDocRef, userData, { merge: true }); // Use merge to not overwrite existing fields
+          console.log("Document written with ID: ", userId);
+        } catch (e) {
+          console.error("Error adding document: ", e);
+        }
+      }
+    }
+
+    addData();
+  }, [user]); // Dependency array to run the effect when `user` changes
+
+  // Return your component's JSX or null if not needed
+  return null;
+}
