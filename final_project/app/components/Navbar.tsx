29c709a5f83730c402b9ed91d845fdefc30153d0
RossLalor <92318461+RossLalor@users.noreply.github.com>
2024-04-18 00:02:43 +0100

    Navbar overhaul again, Model support added
    Added a model viewer to see the inside of an engine. This is on a temporary page for now, more work in the future to come

--- final_project/app/components/Navbar.tsx
+++ final_project/app/components/Navbar.tsx
@@ -1,45 +1,61 @@
-import Link from 'next/link';
-import { UserButton } from '@clerk/nextjs';
+import Link from "next/link";
+import { UserButton } from "@clerk/nextjs";
 import { IoHomeSharp } from "react-icons/io5";
 
 const Navbar = () => {
   return (
     <div className="px-4 py-4 bg-slate-900">
-    <nav className="flex items-center rounded justify-between bg-gradient-to-r from-green-600 via-blue-400 to-blue-600 p-6">
-      <div className="flex items-center flex-shrink-0 text-white mr-6">
-        {/* Place for a logo or title if needed */}
-      </div>
-      <div className="flex gap-8 text-white text-xl">
-        <Link href="/">
-          <div className="hover:text-gray-300 flex-row flex transition duration-300 cursor-pointer left-10 text-3xl absolute"><div><IoHomeSharp /></div><div className="px-4">Leccy Cars</div></div>
+      <nav className="flex rounded  bg-gradient-to-r from-green-600 via-blue-400 to-blue-600 p-6">
+        <Link className="w-8 flex " href="/">
+          <div className="hover:text-gray-300 flex-row flex transition duration-300 cursor-pointer left-10 text-3xl items-center hover:scale-150 transition duration-500">
+            <IoHomeSharp />
+          </div>
         </Link>
-        <Link href="/charts">
-          <div className="hover:text-gray-300 transition duration-300 cursor-pointer">Air Quality Measuring</div>
+        <Link
+          className="w-64 flex hidden md:block"
+          href="/"
+        >
+          <div className="hover:text-gray-300 flex-row flex transition duration-300 cursor-pointer left-10 text-3xl ">
+            <div className="px-4 hidden md:block ">Leccy Cars</div>
+          </div>
         </Link>
+        <div className="flex gap-8 text-white text-xl w-full flex-wrap justify-end items-center">
+          <Link href="/charts">
+            <div className="hover:text-gray-300 transition duration-300 cursor-pointer">
+              Air Quality Measuring
+            </div>
+          </Link>
 
-        <Link href="/electricCars">
-          <div className="hover:text-gray-300 transition duration-300 cursor-pointer">SDG Page</div>
-        </Link>
+          <Link href="/electricCars">
+            <div className="hover:text-gray-300 transition duration-300 cursor-pointer">
+              SDG Page
+            </div>
+          </Link>
 
-        <Link href="/Research">
-          <div className="hover:text-gray-300 transition duration-300 cursor-pointer">Research</div>
-        </Link>
+          <Link href="/Research">
+            <div className="hover:text-gray-300 transition duration-300 cursor-pointer">
+              Research
+            </div>
+          </Link>
 
-        <Link href="/userPage">
-          <div className="hover:text-gray-300 transition duration-300 cursor-pointer">User Page</div>
-        </Link>
+          <Link href="/userPage">
+            <div className="hover:text-gray-300 transition duration-300 cursor-pointer">
+              User Page
+            </div>
+          </Link>
 
-        <Link href="/about">
-          <div className="hover:text-gray-300 transition duration-300 cursor-pointer">About</div>
-        </Link>
-        
-        <div className="hover:text-gray-300 transition duration-300 cursor-pointer">
+          <Link href="/about">
+            <div className="hover:text-gray-300 transition duration-300 cursor-pointer">
+              About
+            </div>
+          </Link>
+          <div></div>
+        </div>
+        <div className="flex justify-end items-center scale-125 hover:scale-150 transition duration-1000">
           <UserButton />
         </div>
-
-      </div>
-    </nav>
-  </div>
+      </nav>
+    </div>
   );
 };
 
--- final_project/app/userPage/page.tsx
+++ final_project/app/userPage/page.tsx
@@ -4,32 +4,46 @@ import { useState, ChangeEvent } from "react";
 import Link from "next/link";
 import dingleDang from "../firebaseAddData";
 import DongieWongies from "../components/firebaseAddData";
+import { GLTFModel, AmbientLight, DirectionLight } from "react-3d-viewer";
 
 // Define the shape of your form data using an interface
 
-
 export default function Home() {
   const { isSignedIn, user } = useUser();
-
-
-
+  const modelPath = "/chevy_camaro_ss.glb";
 
   if (isSignedIn) {
     return (
       <main className="min-h-screen min-w-screen bg-cover bg-slate-900">
         <div className="backdrop-blur-[3px] min-h-screen">
-
           <div className="flex justify-center py-4">
             <div>
               <h1 className="backdrop-blur-md rounded-3xl bg-slate-800 drop-shadow-lg font-extrabold sm:text-[3rem] py-8 px-4 max-w-4xl text-white text-center">
                 <div className="justify-center flex">
-                  Greetings, {user?.firstName} <div className="px-4"><img className="rounded-3xl w-24 h-24justify-center flex "  src={user.imageUrl} alt="user image" /></div>
+                  Greetings, {user?.firstName}{" "}
+                  <div className="px-4">
+                    <img
+                      className="rounded-3xl w-24 justify-center flex "
+                      src={user.imageUrl}
+                      alt="user image"
+                    />
+                  </div>
                   <DongieWongies />
                 </div>
+                <GLTFModel src={modelPath}>
+                  <AmbientLight color={0xffffff} />
+                  <DirectionLight
+                    color={0xffffff}
+                    position={{ x: 100, y: 200, z: 100 }}
+                  />
+                  <DirectionLight
+                    color={0xff00ff}
+                    position={{ x: -100, y: 200, z: -100 }}
+                  />
+                </GLTFModel>
               </h1>
             </div>
           </div>
-
         </div>
       </main>
     );
