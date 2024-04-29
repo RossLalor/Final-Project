5f6112ef7ee405fd80559e7763996d180cd910e9
RossLalor <92318461+RossLalor@users.noreply.github.com>
2024-04-29 15:03:44 +0100

    Updated Research with more research
    Added some AQI analytics to the page to show how going Electric would indeed reduce emissions in the air.

--- final_project/app/Research/page.tsx
+++ final_project/app/Research/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React, { useState } from "react";
-import { Doughnut } from "react-chartjs-2";
-import Chart, { ArcElement, Tooltip, Legend } from "chart.js/auto";
+import { Doughnut, Bar } from "react-chartjs-2";
+import Chart, { ArcElement, Tooltip, Legend, ChartData } from "chart.js/auto";
 
 export default function AirQualityPage() {
   const data = {
@@ -75,6 +75,43 @@ export default function AirQualityPage() {
       },
     ],
   };
+  const AQIData: ChartData<"bar" | "line", number[], string> = {
+    labels: [
+      "Monday 00:00",
+      "Monday 17:00",
+      "Tuesday 00:00",
+      "Tuesday 17:00",
+      "Wednesday 00:00",
+      "Wednesday 17:00",
+      "Thursday 00:00",
+      "Thursday 17:00",
+      "Friday 00:00",
+      "Friday 17:00",
+      "Saturday 00:00",
+      "Saturday 17:00",
+      "Sunday 00:00",
+      "Sunday 17:00",
+    ],
+    datasets: [
+      {
+        type: "bar",
+        label: "Bar Dataset",
+        data: [69, 200, 45, 190, 44, 212, 34, 182, 57, 168, 61, 177, 41, 200],
+        backgroundColor: ["rgba(255, 99, 132, 0.5)"],
+        borderColor: ["rgb(0,0,255)"],
+        borderWidth: 1,
+      },
+      {
+        type: "line",
+        label: "Line Dataset",
+        data: [69, 200, 45, 190, 44, 212, 34, 182, 57, 168, 61, 177, 41, 200],
+        borderColor: "rgb(255,0,0)",
+        backgroundColor: "rgba(75, 192, 192)",
+        tension: 0.4,
+      },
+    ],
+  };
+
   const [showCo2, setShowCo2] = useState(false);
 
   Chart.register(ArcElement, Tooltip, Legend);
@@ -160,6 +197,58 @@ export default function AirQualityPage() {
             quality. The data also shows that diesel cars emit a significant
             amount of CO2, which is a greenhouse gas that contributes to climate
             change.
+          </p>
+        </div>
+        <div className="bg-gray-800 p-6 h-[50vh] rounded-lg shadow-lg mt-4">
+          <div className="font-semibold text-center">
+            AQI over a 7 Day Period in Dublin Ireland
+          </div>
+          <Bar
+            data={AQIData as ChartData<"bar", number[], string>}
+            options={{
+              responsive: true,
+              maintainAspectRatio: false,
+              scales: {
+                x: {
+                  ticks: {
+                    color: "white",
+                  },
+                  grid: {
+                    color: "rgba(255, 255, 255, 0.1)", //adjusts grid line colors
+                  },
+                },
+                y: {
+                  ticks: {
+                    color: "white",
+                  },
+                  grid: {
+                    color: "rgba(255, 255, 255, 0.1)", //adjusts grid line colors
+                  },
+                },
+              },
+            }}
+          />
+        </div>
+        <div className="bg-gray-800 p-6 rounded-lg shadow-lg mt-4">
+          <div className="font-semibold cursor-pointer text-center">
+            AQI Examination
+          </div>
+          <p className="text-gray-400 mt-2 text-center">
+            Plotted over a seven day period in Dublin city (21st April - 29th
+            April), we can clearly see a sinusoidal graph that has developed.
+            This is due to emissions raising and falling as the day goes
+            onwards, particularly around 17:00. I chose to log this time
+            specifically due to it being peak traffic time in Dublin city. This
+            data was sourced from using the API-Ninjas AQI API found on the AQI
+            page. Interestingly, you can see the emissions at midnight raise on
+            Fridays and Saturdays. This is possibly due to night activity being
+            higher on these days and more taxis driving around dublin as a
+            result. Also worth considering is the nightlink buses that are
+            active on the weekends. If every car in Dublin were electric, this
+            graph would be diminished greatly considering that emissions are
+            raising at peak times so drastically. Emissions would not go to zero
+            as things like chimneys and factories would still be running
+            however.
           </p>
         </div>
       </div>
